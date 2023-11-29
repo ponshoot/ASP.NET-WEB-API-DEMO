e7da795100aa8a85ddd55f3ded7d887c0428ed7b
Jones <c187779343@gmail.com>
2023-11-29 16:30:40 +0800

    建立資料庫連線、Customer Api的 Get方法

---
+++ ApiDemo/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using ApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Customers.ToList());
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            if (!_context.Customers.Where(c => c.CustomerId == id).Any())
+                return NotFound();
+            return Ok(_context.Customers.Where(c => c.CustomerId == id).FirstOrDefault());
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- ApiDemo/Models/NorthwindContext.cs
+++ ApiDemo/Models/NorthwindContext.cs
@@ -69,10 +69,6 @@ public partial class NorthwindContext : DbContext
 
     public virtual DbSet<Territory> Territories { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\COSH\\AppData\\Local\\Microsoft\\Microsoft SQL Server Local DB\\Instances\\MSSQLLocalDB\\northwnd.mdf;Integrated Security=True;Connect Timeout=30");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AlphabeticalListOfProduct>(entity =>
