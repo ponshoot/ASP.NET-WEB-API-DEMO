5e254064804e75d3f519a03f3e6eafc241373fa9
Jones <c187779343@gmail.com>
2023-11-30 18:45:35 +0800

    建立 Customer Api的 Post、Put、Delete方法

--- ApiDemo/Controllers/CustomerController.cs
+++ ApiDemo/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using ApiDemo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,20 +35,52 @@ namespace ApiDemo.Controllers
 
         // POST api/<CustomerController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Customer customerCreate)
         {
+            if (customerCreate == null)
+                return BadRequest();
+            var customer = _context.Customers
+                .Where(c => c.CustomerId.Trim().ToUpper() == customerCreate.CustomerId.Trim().ToUpper())
+                .FirstOrDefault();
+            if (customer != null)
+            {
+                ModelState.AddModelError("", "此 Customer 已存在");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Customers.Add(customerCreate);
+            _context.SaveChanges();
+            return Ok("成功建立");
         }
 
         // PUT api/<CustomerController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(string id, [FromBody] Customer customerUpdate)
         {
+            if (customerUpdate == null)
+                return BadRequest();
+            if (!_context.Customers.Where(c => c.CustomerId == id).Any())
+                return BadRequest();
+            _context.Customers.Update(customerUpdate);
+            _context.SaveChanges();
+            return Ok("成功更新");
         }
 
         // DELETE api/<CustomerController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(string id)
         {
+            if (id == null)
+                return BadRequest();
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+                return BadRequest();
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return Ok("成功刪除");
         }
     }
 }
